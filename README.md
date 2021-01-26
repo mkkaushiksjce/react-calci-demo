66052f9221274df2093ffaadb30ef62deab5d985
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-01-26 21:42:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-calci-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-calci-demo)
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./app.css";
+import Result from "./components/result";
+import KeyPad from "./components/keypad";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    };
+  }
+
+  onClick = button => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+
+  calculate = () => {
+    var checkResult = "";
+    if (this.state.result.includes("--")) {
+      checkResult = this.state.result.replace("--", "+");
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(checkResult) || "") + ""
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-body">
+          <h1>KeepWorks Calculator</h1>
+          <Result result={this.state.result} />
+          <KeyPad onClick={this.onClick} />
+          <div className="theme-wrap cf">
+            <div className="light btn f-l" name="Light">
+              Light
+            </div>
+            <div className="dark btn f-r" name="Light">
+              Dark
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
